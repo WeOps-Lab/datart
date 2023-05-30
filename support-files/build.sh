fa6cb4af75c2e29910ed3c9b5d80c2d9a2fd0c9a
umaru <15875339926@139.com>
2023-05-30 15:50:12 +0800

    add pg

--- support-files/build.sh
+++ support-files/build.sh
@@ -1,3 +1,9 @@
 source /etc/profile
 
 mvn clean package -Dmaven.test.skip=true
+mv ./datart-server-*.zip ./support-files/datart-server-install.zip
+
+cd ./support-files
+docker build -t ccr.ccs.tencentyun.com/megalab/datart .
+
+docker push ccr.ccs.tencentyun.com/megalab/datart
